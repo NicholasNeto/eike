b31b022485c503d5f1a67003936e752019189674
Dalton Silva <dalton.silva@finanzero.com.br>
2019-10-29 13:51:41 -0300

    adding interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 const NEWS_API_KEY = '50b1dd50b09b45c6afe263b7790b4c56';
-axios.defaults.params = {
-    'apiKey': NEWS_API_KEY,
-    'country': 'us'
-};
-
 const eikeAxios = axios.create({
     baseURL: 'https://newsapi.org/v2'
 });
 
-
+eikeAxios.interceptors.request.use((config) => {
+    config.url = config.url + "?country=us&apiKey=" + NEWS_API_KEY;
+    return config;
+}, (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+});
 
 export { eikeAxios };
